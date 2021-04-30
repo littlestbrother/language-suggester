0cdcef2fe94e5a0095712905b0e97d1f07c24bf7
Aaron Kauffman <aaron.christian.kauffman@gmail.com>
2021-04-29 17:23:35 -0700

    Remove all but one form in scripts + add points system

--- js/scripts.js
+++ js/scripts.js
@@ -1,61 +1,54 @@
 $(document).ready(function() {
-    let ruby = 0;
-    let csharp = 0;
-    let javaScript = 0;
-    let go = 0;
-    let python = 0;
-    let rust = 0;
-    let swift = 0;
+    //programming language variables
+    let ruby = 0; //1
+    let csharp = 0; //2
+    let javaScript = 0; //3
+    let go = 0; //4
+    let python = 0; //5
+    let rust = 0; //6
+    let swift = 0; //7
 
 
     // on submit instructions for intro form
     $("form#intro").submit(function(event) {
         event.preventDefault();
         console.log("quiz has started")
+            //css manipulation
         $("form#intro").hide();
         $("form#q1").show();
 
     });
-    // q1 functionality to next question
+    // question logic
     $("form#q1").submit(function(event) {
         event.preventDefault();
         let q1 = $('input[name="q1"]:checked').val();
-        console.log(q1);
+        console.log(q1 + " has been selected"); //debug
+
+        //switch function because if and then statments are kinda lame imo.
+        switch (q1) {
+            case 'ruby':
+                ruby = +1 / 5 //this implemements a point system which is what will determine which language is best for the user
+                console.log(ruby + " is added to: " + q1); //debug
+                break; //to stop the case to save computation power.
+            case 'csharp':
+                csharp = +1 / 5
+                console.log(csharp + " is added to: " + q1); //debug
+                break;
+            case 'javaScript':
+                javaScript = +1 / 5
+                console.log(javaScript + " is added to: " + q1); //debug
+                break;
+            case 'go':
+                go = +1 / 5
+                console.log(go + " is added to: " + q1); //debug
+                break;
+            default:
+                alert("Please choose something.")
+        }
+
+        //css manipulation
         $("form#q1").hide();
         $("form#q2").show();
 
     });
-    // q2 functionality to next function
-    $("form#q2").submit(function(event) {
-        event.preventDefault();
-        let q2 = $('input[name="q2"]:checked').val();
-        console.log(q2);
-        $("form#q2").hide();
-        $("form#q3").show();
-    });
-    // q3 functionality to next question
-    $("form#q3").submit(function(event) {
-        event.preventDefault();
-        let q3 = $('input[name="q3"]:checked').val();
-        console.log(q3);
-        $("form#q3").hide();
-        $("form#q4").show();
-    });
-    // q4 functionality to next function
-    $("form#q4").submit(function(event) {
-        event.preventDefault();
-        let q4 = $('input[name="q4"]:checked').val();
-        console.log(q4);
-        $("form#q4").hide();
-        $("form#q5").show();
-    });
-    // q5 functionality to next function
-    $("form#q5").submit(function(event) {
-        event.preventDefault();
-        let q5 = $('input[name="q5"]:checked').val();
-        console.log(q5);
-        $("form#q5").hide();
-        $("form#results").show();
-    });
-
 });
